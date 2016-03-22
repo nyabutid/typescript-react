1be8ab019fc6d88206dc3c17d50fd106d9d23070
devadmin <Dickens Nyabutidickens.nyabuti@optimine.com>
2016-03-22 12:12:43 -0500

    initial commit

---
+++ js/global.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/browser.d.ts" />
+
+import React = __React;
+import ReactDOM = __React.__DOM;
+
+declare namespace __React {
+    //export import addons = __React.__Addons;
+}
